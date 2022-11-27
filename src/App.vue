69866bfb873754145d443d49a45e4c047fc30cd3
diandianjun <1730523754@qq.com>
2022-11-27 15:52:41 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -167,7 +167,7 @@ export default {
     let routerData;
     let pageInfo;
     let timer;
-    const baseUrl='自己服务器的ip';
+    const baseUrl='自己服务器的ip或url';
     let categoryId=0;
     let pictureList=[
         'http://www.diandianjun.com.cn:8080/resource/blog/dora1.jpg',
